7bf9846922a4229fc30f23df93eb383f8ec21dac
mohamedtamer0 <mt4452242@gmail.com>
2022-11-06 21:14:47 +0200

    update

--- routes/users.js
+++ routes/users.js
@@ -35,4 +35,19 @@ router.delete('/:id', (req, res) => {
     res.send(`User with the id ${id} deleted from the database`);
 });
 
+router.patch('/:id', (req,res) => {
+    const {id} = req.params;
+    const {firstName,lastName,age} = req.body;
+    const user = users.find((user) => user.id == id);
+
+    if(firstName) user.firstName = firstName;
+
+    if(lastName) user.lastName = lastName;
+
+    if(age) user.age = age;
+
+    res.send(`User with the id ${id} has been updated`);
+
+});
+
 export default router;
